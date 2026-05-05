47e18ed4507f0b561a9a4dcbd68f1817d49b9ffc
gwanga <wangageremi725@gmail.com>
2026-05-05 12:09:58 +0300

    create more data structures and freeze to ensure immutability

--- declarations.js
+++ declarations.js
@@ -1,1 +1,21 @@
 const escapestr = "`\\/\"'"
+const arr = [4, "2"]
+const obj = {
+    str : "freedom",
+    num : 505,
+    bool: false,
+    undef: undefined
+}
+
+const nested = {
+    arr : [4, undefined, "2"],
+    obj : {
+        str: "chains",
+        num: 505,
+        bool: false,
+    }
+}
+
+Object.freeze(nested)
+Object.freeze(obj)
+Object.freeze(arr)
