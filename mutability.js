13356a3b22f8ec263c0daa377723cc897bbb2854
gwanga <wangageremi725@gmail.com>
2026-05-05 12:58:35 +0300

    omit changing person.name from code

--- mutability.js
+++ mutability.js
@@ -5,11 +5,10 @@ const samePerson = person
 Object.freeze(clone1)
 
 function change(str) {
-    person.name = str
     person.age = person.age+1
     person.country = "FR"
 }
-change("Geremi")
+change()
 
 console.log(person.age === 78); // Should be true
 console.log(person.country === "FR"); // Should be true
@@ -20,7 +19,8 @@ console.log(clone1.country === "US"); // Should be true
 console.log(clone2.age === 77); // Should be true
 console.log(clone2.country === "US"); // Should be true
 
-console.log(samePerson.age === 78); // Should be true
+console.log(samePerson.age === 78);
+console.log(person.name==='Rick') // Should be true
 console.log(samePerson.country === "FR"); // Should be true
 
 
