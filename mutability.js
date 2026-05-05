254c53e342eff76c68ab1b0a89fc1224421a02ec
gwanga <wangageremi725@gmail.com>
2026-05-05 12:48:39 +0300

    add logs for validating data

---
+++ mutability.js
@@ -0,0 +1,31 @@
+const person = {
+    name: "Rick",
+    age: 77,
+    country: "US",
+};
+
+
+const clone1 = {...person}
+const clone2 = {...person}
+const samePerson = person
+
+Object.freeze(clone1)
+
+function change(str) {
+    person.name = str
+    person.age = person.age+1
+    person.country = "FR"
+}
+change("Geremi")
+
+console.log(person.age === 78); // Should be true
+console.log(person.country === "FR"); // Should be true
+
+console.log(clone1.age === 77); // Should be true
+console.log(clone1.country === "US"); // Should be true
+
+console.log(clone2.age === 77); // Should be true
+console.log(clone2.country === "US"); // Should be true
+
+console.log(samePerson.age === 78); // Should be true
+console.log(samePerson.country === "FR"); // Should be true
