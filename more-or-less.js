c86b6a816025e977eb58398ff76bf70f9d446c99
gwanga <wangageremi725@gmail.com>
2026-05-05 13:02:41 +0300

    create functions for arithmetics

---
+++ more-or-less.js
@@ -0,0 +1,21 @@
+function more(arg) {
+    arg += 1
+    return arg
+}
+
+function less(arg){
+    arg-=1
+    return arg
+}
+
+function add(arg1, arg2){
+    let fin =arg1 + arg2
+
+    return fin
+}
+
+function sub(arg1, arg2){
+    let fin = arg1 - arg2
+
+    return fin
+}
