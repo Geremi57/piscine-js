369da473870898e3279f5f027ffe0c8fc955b0f2
gwanga <wangageremi725@gmail.com>
2026-05-06 16:17:55 +0300

    change type of object to be detected by instance of

--- collections.js
+++ collections.js
@@ -80,6 +80,53 @@ const superTypeOf = function(val){
         return 'undefined'
     }else if (val === null){
         return 'Null'
+    }else if (val instanceof Object){
+        return 'Object'
+    }else if (val instanceof String){
+        return 'String'
+    }else if (val instanceof Number){
+        return 'Number'
     }
     return typeof val
 }
+
+
+
+const str = "hello";
+const arr = [1, 2, 1, 3];
+const obj = { x: 45, y: 75, radius: 24 };
+const set = new Set();
+set.add(1);
+set.add(2);
+set.add(1); // Duplicate will be ignored
+set.add(3);
+const map = new Map();
+map.set("a", 1);
+map.set("b", 2);
+map.set(3, "c");
+map.set(4, "d");
+
+// Test each function
+console.log("arrToSet:", arrToSet(arr));
+console.log("arrToStr:", arrToStr(arr));
+console.log("setToArr:", setToArr(set));
+console.log("setToStr:", setToStr(set));
+console.log("strToArr:", strToArr(str));
+console.log("strToSet:", strToSet(str));
+console.log("mapToObj:", mapToObj(map));
+console.log("objToArr:", objToArr(obj));
+console.log("objToMap:", objToMap(obj));
+console.log("arrToObj:", arrToObj(arr));
+console.log("strToObj:", strToObj(str));
+
+// Test superTypeOf with different types
+console.log("superTypeOf(map):", superTypeOf(map));
+console.log("superTypeOf(set):", superTypeOf(set));
+console.log("superTypeOf(obj):", superTypeOf(obj));
+console.log("superTypeOf(str):", superTypeOf(str));
+console.log("superTypeOf(666):", superTypeOf(666));
+console.log("superTypeOf(NaN):", superTypeOf(NaN));
+console.log("superTypeOf(arr):", superTypeOf(arr));
+console.log("superTypeOf(null):", superTypeOf(null));
+console.log("superTypeOf(undefined):", superTypeOf(undefined));
+console.log("superTypeOf(superTypeOf):", superTypeOf(superTypeOf));
