0d28ba2bcc0e847d5452a91a553fddbc8a31d95c
gwanga <wangageremi725@gmail.com>
2026-05-06 16:59:39 +0300

    use the corect input order in hash code helper

--- block-chain.js
+++ block-chain.js
@@ -1,8 +1,7 @@
 const blockChain = (data, prev = { index: 0, hash: '0' }) => {
     const index = prev.index + 1
 
-    const hash = hashCode(JSON.stringify(data) + prev.hash + index)
-
+    const hash = hashCode(index + prev.hash + JSON.stringify(data))
     const block = {
         index,
         hash,
