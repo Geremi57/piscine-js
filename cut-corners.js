3e87cfd12f5799566d7291f551b14fdac6ab63e0
gwanga <geremiwanga57@gmail.com>
2026-05-07 16:40:42 +0300

    trucate decimal when assigning to int32 array to reduce time complexity

--- cut-corners.js
+++ cut-corners.js
@@ -124,9 +124,3 @@ const trunc = function (n) {
     return arr[0]
     // return i;
 }
-
-const nums = [3.7, -3.7, 3.1, -3.1];
-console.log(nums.map(round));
-console.log(nums.map(floor));
-console.log(nums.map(trunc));
-console.log(nums.map(ceil));
