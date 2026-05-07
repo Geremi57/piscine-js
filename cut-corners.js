93d35025cdcc3fb355a79eeefe2e62a7c66bf1e9
gwanga <geremiwanga57@gmail.com>
2026-05-07 17:02:20 +0300

    omit logging to console

--- cut-corners.js
+++ cut-corners.js
@@ -167,9 +167,3 @@ const trunc = function (n) {
     return n & -1 
 }
 
-const nums = [3.7, -3.7, 3.1, -3.1];
-console.log(nums.map(round));
-console.log(nums.map(floor));
-console.log(nums.map(trunc));
-console.log(nums.map(ceil));
-
