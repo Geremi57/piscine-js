fcc5ca52947cd5c8c604230b8e2a9ae4ccb7660f
gwanga <geremiwanga57@gmail.com>
2026-05-09 14:14:19 +0300

    add checker for empty separator

--- unbreakable.js
+++ unbreakable.js
@@ -1,17 +1,41 @@
-const split = function(str, sep){
+const split = function(str, sep) {
     let arr = []
     let word = ""
-    for (let i= 0; i < str.length; i++){
-       if (str[i] == sep) {
+    if (sep == "") {
+    let arr = []
+
+    for (let i = 0; i < str.length; i++) {
+        arr.push(str[i])
+    }
+
+    return arr
+}
+
+    for (let i = 0; i < str.length; i++) {
+
+        let match = true
+
+        for (let j = 0; j < sep.length; j++) {
+            if (str[i + j] != sep[j]) {
+                match = false
+                break
+            }
+        }
+
+        if (match) {
             arr.push(word)
             word = ""
+            i += sep.length - 1
         } else {
             word += str[i]
         }
     }
+
     arr.push(word)
+
     return arr
 }
+
 
 const join = function(arr, sep){
     let str = ""
