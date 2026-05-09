4459fd10e03b045c8fc2db08d4af7093b38bcad2
gwanga <geremiwanga57@gmail.com>
2026-05-09 15:45:14 +0300

    make the loop jump by the sixe

---
+++ chunky.js
@@ -0,0 +1,14 @@
+const chunky = function(arr, n) {
+    let twoDarr = []
+    for (let i = 0; i < arr.length; i+= n) {
+        let start = 0
+        let sliced;
+        // if (i == n) {
+        //     sliced = 
+        //     start = i + 1
+        // }
+        twoDarr.push(arr.slice(i, i + n))
+    }
+    return twoDarr
+}
+\
