b77c1a81584344bb39ff71c0e4a7b83846d87176
gwanga <wangageremi725@gmail.com>
2026-05-10 20:42:03 +0300

    use matchAll to return groups

---
+++ group-price.js
@@ -0,0 +1,10 @@
+const groupPrice = function(str){
+
+    let matches = [...str.matchAll(/[A-Z]{3}(\d+)\.(\d+)/g)]
+
+    return matches.map(match => [
+        match[0],
+        match[1],
+        match[2]
+    ])
+}
