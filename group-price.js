4b52fcb3add0304e825a8888f68f1d9280c87914
gwanga <wangageremi725@gmail.com>
2026-05-10 20:48:06 +0300

    add alternation for in input to allow different currency formats

--- group-price.js
+++ group-price.js
@@ -1,6 +1,6 @@
 const groupPrice = function(str){
 
-    let matches = [...str.matchAll(/[A-Z]{3}(\d+)\.(\d+)/g)]
+    let matches = [...str.matchAll(/(?:[A-Z]{3}|\$)(\d+)\.(\d+)/g)]
 
     return matches.map(match => [
         match[0],
@@ -8,3 +8,5 @@ const groupPrice = function(str){
         match[2]
     ])
 }
+
+console.log(groupPrice('The price of the cereals is $4.00.'));
