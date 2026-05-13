c0eacf7a19b470d97fd0bab99a3929823964efa6
gwanga <wangageremi725@gmail.com>
2026-05-13 17:49:43 +0300

    fix: isValid to check if type is number or dat if not return false

--- date-is.js
+++ date-is.js
@@ -1,27 +1,35 @@
 const isValid = function(date) {
-  return date instanceof Date && !isNaN(date.getTime());
+  const isDateObject = date instanceof Date;
+  const isTimestamp = typeof date === "number";
+
+  if (!isDateObject && !isTimestamp) {
+    return false;
+  }
+
+  return !isNaN(new Date(date).getTime());
 };
 
 const isAfter = function(date1, date2) {
   return isValid(date1) &&
          isValid(date2) &&
-         date1.getTime() > date2.getTime();
+         new Date(date1).getTime() >
+         new Date(date2).getTime();
 };
 
 const isBefore = function(date1, date2) {
   return isValid(date1) &&
          isValid(date2) &&
-         date1.getTime() < date2.getTime();
+         new Date(date1).getTime() <
+         new Date(date2).getTime();
 };
 
 const isFuture = function(date) {
   return isValid(date) &&
-         date.getTime() > Date.now();
+         new Date(date).getTime() > Date.now();
 };
 
 const isPast = function(date) {
   return isValid(date) &&
-         date.getTime() < Date.now();
+         new Date(date).getTime() < Date.now();
 };
-
 console.log(isValid('2013-01-01'));
