9c844f8b27b34ad807e88028cc731146738cf402
gwanga <wangageremi725@gmail.com>
2026-05-14 14:55:39 +0300

    change the round to floor to get the next small interger

--- using-map.js
+++ using-map.js
@@ -19,7 +19,7 @@ const fahrenheitToCelsius = function(arr) {
     let toFahrenheit = arr.map((val) => {
         let act = parseFloat(val)
         console.log(act)
-        return `${Math.round((act - 32) * 5 /9)}°C`
+        return `${Math.floor((act - 32) * 5 /9)}°C`
     })
     return toFahrenheit
 }
@@ -39,7 +39,7 @@ const tempForecasts = function(arr){
         val.state = val.state.charAt(0).toUpperCase() + val.state.slice(1)
 
         val.temperature = parseFloat(val.temperature.trim())
-        val.temperature = `${Math.round((val.temperature - 32) * 5 /9)}°Celcius in ${val.city}, ${val.state}`
+        val.temperature = `${Math.floor((val.temperature - 32) * 5 /9)}°Celcius in ${val.city}, ${val.state}`
         let fin = val.temperature
         return fin
     })
@@ -47,12 +47,4 @@ const tempForecasts = function(arr){
 }
 
 
-console.log(upperCasingStates([  'alabama',
-    'new jersey',
-    'alaska',
-    'new york',
-    'california',
-    'new hampshire',
-    'ohio',
-    'texas',
-    'west virginia']))
+console.log(fahrenheitToCelsius(['86°F', '100°F', '41°F', '55°F', '10°F', '70°F', '-2°F']))
