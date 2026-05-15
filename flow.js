6163aac1733df80cec381e22c2aeafd62eae5dc3
gwanga <wangageremi725@gmail.com>
2026-05-16 02:18:23 +0300

    omit variable and logs

--- flow.js
+++ flow.js
@@ -15,9 +15,3 @@ return function(...args) {
         return curr;
     };
 };
-
-const square = (nbr) => nbr * nbr;
-const add2Numbers = (nbr1, nbr2) => nbr1 + nbr2;
-
-const flowedFunctions = flow([add2Numbers, square]);
-console.log(flowedFunctions(2, 3)) // -> 25
