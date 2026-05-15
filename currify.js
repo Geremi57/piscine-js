2e64cb2ab86ad0edeece14daaa4342f00cda5900
gwanga <wangageremi725@gmail.com>
2026-05-16 02:45:31 +0300

    check if we have arguments collect and combine

---
+++ currify.js
@@ -0,0 +1,19 @@
+function currify(callback) {
+    return function collect(...args) {
+        if (args.length >= callback.length) {
+
+            return callback(...args);
+        }
+        return function(...otherArgs) {
+            const combined = [...args,...otherArgs];
+
+            return collect(...combined);
+        };
+    };
+}
+
+const mult2 = (el1, el2) => el1 * el2;
+console.log(mult2(2, 2)); // -> 4
+
+const mult2Curried = currify(mult2);
+console.log(mult2Curried(2)(2)); 
