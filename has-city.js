1ef5d963599889226918648107e06511e1b028f4
gwanga <wangageremi725@gmail.com>
2026-05-16 01:43:50 +0300

    return another function that will receive the city name

---
+++ has-city.js
@@ -0,0 +1,13 @@
+const hasCity = function(country, cities) { 
+   return function(city) {
+        const city =
+
+            cities.includes(ct);
+        
+            if (city) {
+                return `${ct} is a city from ${country}`;
+        
+            }
+        return `${ct} is not a city from ${country}`;
+    };
+};
