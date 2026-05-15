212dab6ccf7fef82dd90e50c00975f291ceecfa7
gwanga <wangageremi725@gmail.com>
2026-05-16 00:26:33 +0300

    minus 2 from length of array in reduce right in backward loop

---
+++ reduce.js
@@ -0,0 +1,41 @@
+const fold = function(arr, callback, acc){
+    if (acc == undefined) {
+        acc = 0
+    }
+    for (let i = 0; i < arr.length; i++){
+        acc = callback(acc, arr[i], arr)
+    }
+    return acc
+}
+
+const foldRight = function(arr, callback, acc){
+     if (acc == undefined) {
+        acc = 0
+    }
+    for (let i = arr.length; i > 0; i--){
+        acc = callback(acc, arr[i], arr)
+    }
+    return acc
+}
+
+function reduce(arr, callback) {
+    let acc = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+
+        const curr = arr[i];
+
+        acc = callback(acc,curr,i,arr);
+    }
+    return acc;
+}
+
+const reduceRight = function(arr, callback){
+     let acc = arr[arr.length-1]
+    for (let i = arr.length-1-1; i > 0; i--) {
+
+        const curr = arr[i];
+
+        acc = callback(acc,curr,i,arr);
+    }
+    return acc;
+}
