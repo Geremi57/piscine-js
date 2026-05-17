53ada571047cf70ca5324cfcc6f39a902f82c546
gwanga <wangageremi725@gmail.com>
2026-05-18 00:26:19 +0300

    omit import path remove mode by deprecating the current style with unpimp

---
+++ pimp-my-style.js
@@ -0,0 +1,26 @@
+// import { styles } from './pimp-my-style.data.js'
+let currentStyle = 0
+
+export const pimp = () => {
+
+    const button =document.querySelector('button')
+    
+    if (button.classList.contains('unpimp')) {
+        
+        currentStyle--
+        button.classList.remove(styles[currentStyle])
+
+        if (currentStyle === 0) {
+            button.classList.remove('unpimp')
+        }
+    }
+
+    else {
+        button.classList.add(styles[currentStyle])
+        currentStyle++
+
+        if (currentStyle === styles.length) {
+            button.classList.add('unpimp')
+        }
+    }
+}
