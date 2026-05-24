033ab37278aa0beaf70e8edd1df828caa561d494
--global <--global>
2026-05-24 23:43:32 +0300

    remove promise.all and use loop instead

--- all.js
+++ all.js
@@ -1,20 +1,28 @@
 const all = async function(objs = {}) {
-    //const res = {}
+    const res = {}
     //for (const key in objs) res[key] = await objs[key]
 
-    const keys = Object.keys(objs)
-        //console.log(keys)
-    const vals = await Promise.all(Object.values(objs))
-        console.log(vals)
+    // const keys = Object.keys(objs)
+    //     //console.log(keys)
+    // const vals = await Promise.all(Object.values(objs))
+    //     console.log(vals)
 
-    return Object.fromEntries(keys.map((k, i) => [k, vals[i]]))
+    // return Object.fromEntries(keys.map((k, i) => [k, vals[i]]))
+
+    for (const k in objs) {
+        //console.log(k, objs[k])
+        res[k] = await objs[k]
+    }
+
+    return res
+
 }
-
 const obj = {
-  a: Promise.resolve("ok"),
-  b: Promise.reject("fail"),
+  first: Promise.resolve(1),
+  second: Promise.resolve(2),
+  third: 3,
 };
 
-all(obj).catch(console.error);
+all(obj).then(console.log);
 
 // all(obj)
