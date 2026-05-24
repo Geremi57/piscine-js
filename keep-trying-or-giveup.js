1d0ad5adb0d65647904f6ce74912e3654a1aa218
--global <--global>
2026-05-24 23:19:03 +0300

    create timeout to race function against timer

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -15,6 +15,27 @@ const retry  = function (attempts = 3, fn = async () => {}) {
 }
 
 
+const timeout = function (ms = 0, fn = async () => {}) {
+  return async function (...a) {
+      const t = new Promise((res) =>
+      setTimeout(res, ms, Error("timeout"))
+    )
+      //const t = new Promise((res) => setTimeout(res, ms))
+    const call = new Promise((res) => res(fn(...a)))
+
+    //console.log('racing')
+
+    const r = await Promise.race([t, call]).then((r) => r)
+      //const r = await Promise.race([t, call])
+
+    if (r instanceof Error) {
+      throw r
+    }
+
+      return r
+  }
+}
+
 
 const unstableTask = async (x) => {
   if (Math.random() < 0.7) throw new Error("Failed!");
