4ec0a44fbf4f9c529ce29351715a5b76af50c4f6
--global <--global>
2026-05-24 22:52:01 +0300

    fire callback once by adding break

--- interpolation.js
+++ interpolation.js
@@ -20,6 +20,7 @@ const { step, start, end, callback, duration, waitTime = 15 } = obj;
               console.log(cx, cy, waitTime)
             //if (hits === 0) {
             callback([cx, cy]);
+            break
           }
         }
 
@@ -30,8 +31,37 @@ const { step, start, end, callback, duration, waitTime = 15 } = obj;
     hits++
     //hits = hits + 1
 }
+
+let occ = 0;
+
+function interpolation2(obj = {}) {
+    const { step, start, end, callback, duration, waitTime = 15 } = obj;
+    const nrY = duration / step;
+    const nrX = duration > step && end > start ? nrY * 0.1 : nrY;
+
+    for (let i = 0; i < step; i++) {
+        const resX = start + (start > end ? -2 * i + nrX * i : nrX * i);
+        const resY = nrY * (i + 1);
+
+        if (resY < waitTime && occ !== 1) {
+            console.log(resX, resY, waitTime);
+            callback([resX, resY]);
+        }
+    }
+
+    occ++;
+}
 // interpolation()
-interpolation({
+// interpolation({
+//   step: 5,
+//   start: 0,
+//   end: 1,
+//   duration: 10,
+//   callback: (point) => console.log(point),
+// });
+
+
+interpolation2({
   step: 5,
   start: 0,
   end: 1,
