72d1a65990579686451883207b4f68529f9c1286
--global <--global>
2026-05-24 23:09:36 +0300

    use for loop replacing while

--- interpolation.js
+++ interpolation.js
@@ -7,34 +7,26 @@ const { step, start, end, callback, duration, waitTime = 15 } = obj;
     //const x = y * 0.1
     //const x = y
 
-    let i = 0
-    while (i < step) {
+    // let i = 0
+    for (let i = 0; i < step; i++) {
       const cx = start + (start > end ? -2 * i + x * i : x * i);
     const cy = y * (i + 1);
         //const cy = y * i
         //const cy = y + i
 
-        
-        if (cy < waitTime) {
-            if (hits !== 1) {
+        if (cy < waitTime && hits !== 1) {
+
               console.log(cx, cy, waitTime)
             //if (hits === 0) {
             callback([cx, cy]);
-            if (duration > waitTime) break
-            // break
+            // if (duration > waitTime) break
           }
-        }
-
-      i++
     }
 
     //console.log(hits)
     hits++
     //hits = hits + 1
 }
-
-let occ = 0;
-
 // interpolation()
 interpolation({
   step: 5,
@@ -43,4 +35,16 @@ interpolation({
   duration: 10,
   callback: (point) => console.log(point),
 });
+
+
+
+//  const { length } = interpolation({  step: 5,
+//   start: 0,
+//   end: 1,
+//   duration: 10,
+//   callback: (point) => console.log(point), });
+
+// //   return eq(length, 1)
+//   console.log(length, 1)
+
 
