cb55ffdb29e20522bb0059bfe1192766baaa855e
--global <--global>
2026-05-24 22:41:29 +0300

    add waittime in log and place inside condition to fire coords at every step

---
+++ interpolation.js
@@ -0,0 +1,40 @@
+let hits = 0;
+//let hits = -1
+function interpolation(obj = {}) {
+const { step, start, end, callback, duration, waitTime = 15 } = obj;
+    const y = duration / step;
+        const x = duration > step && end > start ? y * 0.1 : y;
+    //const x = y * 0.1
+    //const x = y
+
+    let i = 0
+    while (i < step) {
+      const cx = start + (start > end ? -2 * i + x * i : x * i);
+    const cy = y * (i + 1);
+        //const cy = y * i
+        //const cy = y + i
+
+        
+        if (cy < waitTime) {
+            if (hits !== 1) {
+              console.log(cx, cy, waitTime)
+            //if (hits === 0) {
+            callback([cx, cy]);
+          }
+        }
+
+      i++
+    }
+
+    //console.log(hits)
+    hits++
+    //hits = hits + 1
+}
+// interpolation()
+interpolation({
+  step: 5,
+  start: 0,
+  end: 1,
+  duration: 10,
+  callback: (point) => console.log(point),
+});
